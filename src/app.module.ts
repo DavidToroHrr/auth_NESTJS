734cc20ec548aab11cc80f920839e215fc21d78a
David Toro <davidesteban0736@.com>
2025-03-30 00:19:42 -0500

    se implementa el envío de SMS

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { TwilioModule } from 'nestjs-twilio';
+import { SmsModule } from './sms/sms.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+    }),
+    UsersModule
+  ],
+})
+export class AppModule {}
